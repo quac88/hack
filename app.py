8156ef9ea02ffb8a606ea5440a11d01b986d18ee
“quac88” <mac2@thrasher.com>
2024-01-27 13:12:02 -0600

    feat: create the flask server to listen for base64 on 8888

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, jsonify
+from run import add_gaussian_noise_and_save
+
+app = Flask(__name__)
+
+@app.route('/process-image', methods=['POST'])
+def process_image():
+    # receive the base64 encoded image
+    image_base64 = request.json['image']
+
+    # process the image
+    processed_image_base64 = add_gaussian_noise_and_save(image_base64)
+
+    # Return the processed image as base64 string
+    return jsonify({'image': processed_image_base64})
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=8888)  # Listen on port 8888 for HTTP traffic
