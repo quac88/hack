0ec845c66bcce50553c09b48078c54379049fe68
“quac88” <mac2@thrasher.com>
2024-01-27 15:19:31 -0600

    feat: add static endpoint

--- app.py
+++ app.py
@@ -1,18 +1,31 @@
 from flask import Flask, request, jsonify
-from run import add_gaussian_noise_and_save
+from run import add_gaussian_noise_and_save_dynamic, add_gaussian_noise_and_save_static
 
 app = Flask(__name__)
 
-@app.route('/process-image', methods=['POST'])
+@app.route('/api/processImageDynamic', methods=['POST'])
 def process_image():
     # receive the base64 encoded image
     image_base64 = request.json['image']
 
     # process the image
-    processed_image_base64 = add_gaussian_noise_and_save(image_base64)
+    processed_image_base64 = add_gaussian_noise_and_save_dynamic(image_base64)
+
+    # Return the processed image as base64 string
+    return jsonify({'image': processed_image_base64})
+
+@app.route('/api/processImageStatic', methods=['POST'])
+def process_image_static():
+    # receive the base64 encoded image
+    image_base64 = request.json['image']
+
+    # process the image with fixed sigma
+    processed_image_base64 = add_gaussian_noise_and_save_static(image_base64)
 
     # Return the processed image as base64 string
     return jsonify({'image': processed_image_base64})
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=3000)  # Listen on port 8888 for HTTP traffic
+
+
--- run.py
+++ run.py
@@ -18,12 +18,12 @@ def randomize_sigma():
     sigma = np.random.randint(_min, _max)
     return sigma 
 
-def add_gaussian_noise_and_save(
+def add_gaussian_noise_and_save_dynamic(
     image_base64, mean=0, save_path="image.png"
 ):
     # Randomize sigma
     sigma = randomize_sigma()
-    
+
     # Decode base64 image to NumPy array
     image_bytes = base64.b64decode(image_base64)
     image_pil = Image.open(BytesIO(image_bytes))
@@ -53,3 +53,40 @@ def add_gaussian_noise_and_save(
     cv2.imwrite(save_path, cv2.cvtColor(image_np, cv2.COLOR_RGB2BGR))
 
     return save_path
+
+def add_gaussian_noise_and_save_static(
+    image_base64, mean=0, save_path="image.png"
+):
+    # Randomize sigma
+    sigma = 1000
+
+    # Decode base64 image to NumPy array
+    image_bytes = base64.b64decode(image_base64)
+    image_pil = Image.open(BytesIO(image_bytes))
+    image_np = np.array(image_pil)
+
+    # Convert the image to PyTorch tensor and move to GPU
+    image_tensor = torch.tensor(image_np).float().to(cuda)
+
+    masks = mask_generator.generate(image_np)
+    for mask in masks:
+        segment = mask["segmentation"]
+
+        # Convert the mask to tensor and move to GPU
+        segment_tensor = torch.tensor(segment).to(cuda)
+
+        # Add Gaussian noise only where the mask is True
+        noise_tensor = torch.normal(mean, sigma, image_tensor.shape).to(cuda)
+        image_tensor[segment_tensor] += noise_tensor[segment_tensor]
+
+        # Clip the pixel values to be in the valid range [0, 255]
+        image_tensor = torch.clamp(image_tensor, 0, 255)
+
+    # Convert the image back to CPU and NumPy for saving
+    image_np = image_tensor.to('cpu').numpy().astype(np.uint8)
+
+    # Save the noisy image to the local filesystem
+    cv2.imwrite(save_path, cv2.cvtColor(image_np, cv2.COLOR_RGB2BGR))
+
+    return save_path
+
